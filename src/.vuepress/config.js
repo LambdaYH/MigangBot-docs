de3a64818b68058cfa9cd18860e7602e09f5fbc0
LambdaYH <chenyihang1998@outlook.com>
2023-04-03 15:43:14 +0800

    docs: first commit

---
+++ src/.vuepress/config.js
@@ -0,0 +1,63 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: '米缸',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: '基于Nonebot2的米缸Bot',
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#9AD3F0' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: '使用文档',
+        link: '/docs_for_user/',
+      },
+    ],
+    sidebar: {
+      '/docs_for_user/': [
+        {
+          title: '插件列表',
+          collapsable: false,
+          children: [
+            '',
+            'plugins_for_manage',
+            'plugins',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroImage: https://image.cinte.cc/i/2023/04/03/642a70711a23f.png
+tagline: 基于Nonebot2的米缸Bot
+actionText: 使用文档 →
+actionLink: /docs_for_user/
+footer: Made by mobai with ❤️
+---
